f00d24a2f18e3cf6818f7f4dbab85f489144f327
zybqw <zybqw@qq.com>
2025-01-14 21:03:58 +0800

    data类型提示支持

--- Aumiao-py/src/base/data.py
+++ Aumiao-py/src/base/data.py
@@ -1,6 +1,6 @@
 import json
 from os import getcwd, path
-from typing import Any, cast
+from typing import Any, TypedDict, cast
 
 from . import file as File
 from .decorator import singleton
@@ -16,7 +16,7 @@ class SyncDict(dict[str, Any]):
 	自定义字典类,用于在修改内容时自动同步到文件中.
 	"""
 
-	def __init__(self, file_path: str, parent_ref: dict[str, Any], key: str | None, *args, **kwargs):
+	def __init__(self, file_path: str, parent_ref: Any, key: str | None, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.file_path = file_path
 		self.parent_ref = parent_ref  # 父级字典的引用
@@ -49,6 +49,111 @@ class SyncDict(dict[str, Any]):
 		self._sync_to_file()
 
 
+class AccountData(TypedDict):
+	author_level: str
+	create_time: str
+	description: str
+	id: str
+	identity: str
+	nickname: str
+	password: str
+
+
+class InfoData(TypedDict):
+	e_mail: str
+	nickname: str
+	qq_number: int
+
+
+class BlackRoomData(TypedDict):
+	post: list[str]
+	user: list[str]
+	work: list[str]
+
+
+class UserData(TypedDict):
+	ads: list[str]
+	answers: list[dict[str, str]]
+	black_room: BlackRoomData
+	comments: list[str]
+	emojis: list[str]
+	replies: list[str]
+
+
+class _CodeMaoData(TypedDict):
+	ACCOUNT_DATA: AccountData
+	INFO: InfoData
+	USER_DATA: UserData
+
+
+class DefaultAction(TypedDict):
+	action: str
+	name: str
+
+
+class ClientParameter(TypedDict):
+	all_read_type: list[str]
+	clear_ad_exclude_top: bool
+	cookie_check_url: str
+	get_works_method: str
+	password_login_method: str
+
+
+class Parameter(TypedDict):
+	CLIENT: ClientParameter
+
+
+class ExtraBody(TypedDict):
+	enable_search: bool
+
+
+class More(TypedDict):
+	extra_body: ExtraBody
+	stream: bool
+
+
+class DashscopePlugin(TypedDict):
+	model: str
+	more: More
+
+
+class Plugin(TypedDict):
+	DASHSCOPE: DashscopePlugin
+	prompt: str
+
+
+class ProgramHeaders(TypedDict):
+	Content_Type: str
+	User_Agent: str
+
+
+class Program(TypedDict):
+	AUTHOR: str
+	HEADERS: ProgramHeaders
+	MEMBER: str
+	SLOGAN: str
+	TEAM: str
+	VERSION: str
+
+
+class _CodeMaoSetting(TypedDict):
+	DEFAULT: list[DefaultAction]
+	PARAMETER: Parameter
+	PLUGIN: Plugin
+	PROGRAM: Program
+
+
+class _CodeMaoCache(TypedDict):
+	collected: int
+	fans: int
+	level: int
+	liked: int
+	nickname: str
+	timestamp: int
+	user_id: int
+	view: int
+
+
 @singleton
 class CodeMaoData:
 	def __init__(self) -> None:
@@ -57,13 +162,8 @@ class CodeMaoData:
 		# 父级字典引用
 		self.data = _data
 		# 用 SyncDict 包装子字典
-		self.ACCOUNT_DATA = SyncDict(
-			file_path=DATA_FILE_PATH, parent_ref=self.data, key="ACCOUNT_DATA", **self.data["ACCOUNT_DATA"]
-		)
-		self.USER_DATA = SyncDict(
-			file_path=DATA_FILE_PATH, parent_ref=self.data, key="USER_DATA", **self.data["USER_DATA"]
-		)
-		self.INFO = SyncDict(file_path=DATA_FILE_PATH, parent_ref=self.data, key="INFO", **self.data["INFO"])
+		self.data = SyncDict(file_path=DATA_FILE_PATH, parent_ref=self.data, key=None, **self.data)
+		self.data = cast(_CodeMaoData, self.data)
 
 
 @singleton
@@ -74,15 +174,14 @@ class CodeMaoCache:
 		# 父级字典引用
 		self.cache = _cache
 		# 用 SyncDict 包装子字典
-		self.CACHE = SyncDict(file_path=CACHE_FILE_PATH, parent_ref=self.cache, key=None, **self.cache)
+		self.cache = SyncDict(file_path=CACHE_FILE_PATH, parent_ref=self.cache, key=None, **self.cache)
+		self.cache = cast(_CodeMaoCache, self.cache)
 
 
 @singleton
 class CodeMaoSetting:
 	def __init__(self) -> None:
-		data = File.CodeMaoFile().file_load(path=SETTING_FILE_PATH, type="json")
-		data = cast(dict[str, Any], data)
-		self.DEFAULT = data["DEFAULT"]
-		self.PARAMETER = data["PARAMETER"]
-		self.PLUGIN = data["PLUGIN"]
-		self.PROGRAM = data["PROGRAM"]
+		_setting = File.CodeMaoFile().file_load(path=SETTING_FILE_PATH, type="json")
+		_setting = cast(dict[str, Any], _setting)
+		self.setting = _setting
+		self.setting = cast(_CodeMaoSetting, self.setting)
